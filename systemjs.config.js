96a6a62dd1eb25ee11fd58f16cfc44d0346574f2
Keir Staple <keirstaple@comcast.net>
2016-07-12 17:14:34 -0700

    set up initial files

---
+++ systemjs.config.js
@@ -0,0 +1,62 @@
+/**
+ * System configuration for Angular 2 samples
+ * Adjust as necessary for your application needs.
+ */
+(function(global) {
+  // map tells the System loader where to look for things
+  //added 'moment': 'node_modules/moment/moment.js' from the System.config() that was initially in index.html
+  
+  var baseUrl = '/app';
+  var map = {
+    'app':                        'app', // 'dist',
+    '@angular':                   'node_modules/@angular',
+    'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
+    'rxjs':                       'node_modules/rxjs',
+    'moment':                     'node_modules/moment/moment.js',
+    'ng2-bootstrap':              'node_modules/ng2-bootstrap',
+    'ng2-page-scroll':            'node_modules/ng2-page-scroll',
+    'd3':                         'node_modules/d3'
+  };
+
+  // packages tells the System loader how to load when no filename and/or no extension
+  //solution: removed 'router' from ngPackageNames and added '@angular/router': { main: 'index.js', defaultExtension: 'js' } to packages.
+
+  var packages = {
+    'app':                        { main: 'main.js',  defaultExtension: 'js' },
+    'rxjs':                       { defaultExtension: 'js' },
+    'angular2-in-memory-web-api': { main: 'index.js', defaultExtension: 'js' },
+    '@angular/router':            { main: 'index.js', defaultExtension: 'js' },
+    'ng2-bootstrap':              { main: 'ng2-bootstrap.js', defaultExtension: 'js'},
+    'ng2-page-scroll':            { main: 'ng2-page-scroll.js', defaultExtension: 'js'},
+    'd3':                         { main: 'build/d3.js', defaultExtenstion: 'js' }
+  };
+  var ngPackageNames = [
+    'common',
+    'compiler',
+    'core',
+    'forms',
+    'http',
+    'platform-browser',
+    'platform-browser-dynamic',
+    'router-deprecated',
+    'upgrade',
+  ];
+  // Individual files (~300 requests):
+  function packIndex(pkgName) {
+    packages['@angular/'+pkgName] = { main: 'index.js', defaultExtension: 'js' };
+  }
+  // Bundled (~40 requests):
+  //solution: remove / from beginning of bundles/ so that there aren't doulbe /'s
+  function packUmd(pkgName) {
+    packages['@angular/'+pkgName] = { main: 'bundles/' + pkgName + '.umd.js', defaultExtension: 'js' };
+  }
+  // Most environments should use UMD; some (Karma) need the individual index files
+  var setPackageConfig = System.packageWithIndex ? packIndex : packUmd;
+  // Add package entries for angular packages
+  ngPackageNames.forEach(setPackageConfig);
+  var config = {
+    map: map,
+    packages: packages
+  };
+  System.config(config);
+})(this);
