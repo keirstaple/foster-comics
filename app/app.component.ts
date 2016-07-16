747b56d4695136b137b1ddf44adb0b0a183565bc
Keir Staple <keirstaple@comcast.net>
2016-07-15 21:15:17 -0700

    add routes to all pages and create router links, but still need to create individual components

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { ROUTER_DIRECTIVES } from '@angular/router';
 import { LandingPageComponent } from './landing-page.component';
+import { CharacterBiosComponent } from './character-bios.component';
 import { AboutAuthorComponent } from './about-author.component';
+import { StoreComponent } from './store.component';
+import { ShoppingCartComponent } from './shopping-cart.component';
 
 @Component({
   selector: 'my-app',
   template: `
     <nav>
       <a [routerLink]="['']">Home</a>
+      <a [routerLink]="['character-bios']">Character Bios</a>
       <a [routerLink]="['about-author']">About Author</a>
+      <a [routerLink]="['store']">Store</a>
+      <a [routerLink]="['shopping-cart']">Shopping Cart</a>
     </nav>
     <router-outlet></router-outlet>
   `,
--- app/app.routes.ts
+++ app/app.routes.ts
@@ -1,11 +1,17 @@
 import { provideRouter, RouterConfig } from '@angular/router';
 import { LandingPageComponent } from './landing-page.component';
+import { CharacterBiosComponent } from './character-bios.component';
 import { AboutAuthorComponent } from './about-author.component';
+import { StoreComponent } from './store.component';
+import { ShoppingCartComponent } from './shopping-cart.component';
 
 //the component that's initially loaded by the path '' can't be AppComponent, otherwise everything will load twice. It must be something like Home (or LandingPage)
 export const routes: RouterConfig = [
   { path: '', component: LandingPageComponent },
-  { path: 'about-author', component: AboutAuthorComponent }
+  { path: 'character-bios', component: CharacterBiosComponent },
+  { path: 'about-author', component: AboutAuthorComponent },
+  { path: 'store', component: StoreComponent },
+  { path: 'shopping-cart', component: ShoppingCartComponent }
 ];
 
 //this will be imported in main.ts and passed into bootstrap(). Important to only bootstrap APP_ROUTER_PROVIDERS and not provideRouter().
