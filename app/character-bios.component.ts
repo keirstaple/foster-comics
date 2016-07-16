d645adf7d3496966b637c210667adadff0e78abe
Keir Staple <keirstaple@comcast.net>
2016-07-15 23:58:42 -0700

    add components for character bios, store and shopping cart

---
+++ app/character-bios.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'character-bios',
+  template: `
+    <h1>Character Bios</h1>
+  `
+})
+
+export class CharacterBiosComponent{ }
---
+++ app/shopping-cart.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shopping-cart',
+  template: `
+    <h1>Shopping Cart</h1>
+  `
+})
+
+export class ShoppingCartComponent { }
