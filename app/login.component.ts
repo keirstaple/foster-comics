f186a26b47a227df6685703f3e40259a485d5b42
Keir Staple <keirstaple@comcast.net>
2016-07-18 19:44:53 -0700

    add cancel button to log in template and component that redirects home

--- app/login.component.ts
+++ app/login.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { Auth }      from './auth.service';
+import { ROUTER_DIRECTIVES } from '@angular/router';
 
 @Component({
   selector: 'login',
-  templateUrl: 'app/login.template.html'
+  templateUrl: 'app/login.template.html',
+  directives: [ ROUTER_DIRECTIVES ]
 })
 
 export class LoginComponent {
